d9c0c5e84a730919c198aa3cd74615f6957f3134
Andor <andor@c-leungs-macbook-pro-2.local>
2010-02-27 22:33:06 -0500

    added webide to pjs 0.6

---
+++ webide/sketches/rot_red_cube.js
@@ -0,0 +1,27 @@
+//
+// Rotating Red Cube
+//
+
+float r = 0;
+
+void setup()
+{
+  size(100,500,P3D);
+
+  noFill();
+  strokeWeight(3);
+}
+
+void draw()
+{
+  background(0,0,0);
+
+  translate(50,250,340);
+  rotateZ(frameCount/20);
+  rotateY(r += 0.02);
+  rotateX(r -= 0.03);
+
+  stroke(5 + frameCount%200,0,0);
+
+  box(10,10,10);
+}
