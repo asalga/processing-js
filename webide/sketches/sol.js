9490ba9ed93e927bef35c3c0a2b96039cc741d9e
Andor <andor@c-leungs-macbook-pro-2.local>
2010-03-25 23:27:26 -0400

    updated sol to include shininess.

--- webide/sketches/sol.js
+++ webide/sketches/sol.js
@@ -1,48 +1,57 @@
 //
 // Sol by Andor Salga
 //
+import processing.opengl.*;
 
 float rotEarth = 0.0;
 float rotMoon = 0.0;
+float zoom = 0;
+PFont infoFont;
+PFont starFont;
 
+/*
+*/
 void setup(){
-  size(200,200,P3D);
+  size(400,400,OPENGL);
+  noStroke();
 }
 
+/*
+*/
 void draw()
 {
   // clear the color buffer
   background(0);
-  noStroke();
-  directionalLight( 255, 255, 255,   0, 1, 0 );
-  ambientLight( 128, 255, 255 );
+  
+  lightSpecular(255,255,255);
+  pointLight(200,200,200,0,100,400);
+
+  specular(204, 102, 0);
+  shininess(8.0);
 
   // push matrix to center of the viewport
   pushMatrix();
-  translate( width/2, height/2, 140 );
-
-  // draw the Sun
-  fill( 255, 0, 0 );
-  sphere( 5.0 );
-
+  translate(width/2, height/2, 150 + zoom);
+  
+  // draw the sun
+  fill(255,0,0);
+  sphere(20.0);
+  
   // push on another matrix and rotate and trans
   pushMatrix();
-  rotateY( rotEarth += 0.01 );
-  translate( 0, 0, 10 );
-
-  fill( 25, 70, 20 );
-
-  // draw the Earth
-  sphereDetail( 16 );
-  sphere( 2.5 );
-
+  rotateY(rotEarth += 0.01);
+  translate(0,0,40);
+  
+  fill(25,70,20);
+  
+  sphereDetail(int(zoom + 16));
+  sphere(10);
+  
   pushMatrix();
-  rotateY( rotMoon += 0.08 );
-  translate( 0, 0, 3.5 );
-
-  // draw the Moon
-  fill( 128, 128, 128 );
-  sphere( 0.5 );
+  rotateY(rotMoon += 0.08);
+  translate(0,0,14);
+  fill(128,128,128);
+  sphere(2.5);
 
   popMatrix();
   popMatrix();
