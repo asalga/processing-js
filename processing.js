f96623618b9895cfe2c8e110e7ee4b62c50c8ae5
Andor Salga <andor.salga@senecac.on.ca>
2011-09-08 12:01:50 -0400

    started fix on newer branch.

--- processing.js
+++ processing.js
@@ -10794,25 +10794,32 @@
      * @param {float} far    maximum distance from the origin away from the viewer
      */
     p.ortho = function(left, right, bottom, top, near, far) {
-      if (arguments.length === 0) {
+      if (arguments.length === 4) {
+        near = cameraNear;
+        far = cameraFar;
+      }
+      else if (arguments.length === 0) {
         left = 0;
         right = p.width;
         bottom = 0;
         top = p.height;
-        near = -10;
-        far = 10;
+        near = cameraNear;
+        far = cameraFar;
       }
-
+      
       var x = 2 / (right - left);
       var y = 2 / (top - bottom);
       var z = -2 / (far - near);
 
       var tx = -(right + left) / (right - left);
-      var ty = -(top + bottom) / (top - bottom);
+      var ty =  -(top + bottom) / (top - bottom);
       var tz = -(far + near) / (far - near);
 
       projection = new PMatrix3D();
-      projection.set(x, 0, 0, tx, 0, y, 0, ty, 0, 0, z, tz, 0, 0, 0, 1);
+      projection.set( x, 0, 0, tx,
+                      0, -y, 0, ty,
+                      0, 0, z, tz,
+                      0, 0, 0, 1);
 
       var proj = new PMatrix3D();
       proj.set(projection);
@@ -10823,8 +10830,8 @@
       uniformMatrix("projection3d", programObject3D, "projection", false, proj.array());
       curContext.useProgram(programObjectUnlitShape);
       uniformMatrix("uProjectionUS", programObjectUnlitShape, "uProjection", false, proj.array());
-      frustumMode = false;
     };
+    
     /**
      * The printProjection() prints the current projection matrix to the text window.
      */
